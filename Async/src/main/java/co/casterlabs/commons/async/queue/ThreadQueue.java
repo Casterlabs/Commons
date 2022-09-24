e779ac8f0d9d58783b57d76efcded7b5b83d6006
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-24 01:39:56 -0500

    Async: Move the two queues into their own subpackage.

--- Async/src/main/java/co/casterlabs/commons/async/queue/ThreadQueue.java
+++ Async/src/main/java/co/casterlabs/commons/async/queue/ThreadQueue.java
@@ -9,13 +9,15 @@ http://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-package co.casterlabs.commons.async;
+package co.casterlabs.commons.async.queue;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.NoSuchElementException;
 import java.util.function.Supplier;
 
+import co.casterlabs.commons.async.AsyncTask;
+import co.casterlabs.commons.async.Promise;
 import lombok.NonNull;
 
 /**
