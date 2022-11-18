f5287d7fc2b0fb028711c6dddc754e2bc7e0b500
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-18 13:39:16 -0600

    Platform: Remove the hard-coded word size and detect it on-the-fly.
    Also moved the endianess helper to the Platform class.

--- Platform/src/main/java/co/casterlabs/commons/platform/Arch.java
+++ Platform/src/main/java/co/casterlabs/commons/platform/Arch.java
@@ -11,44 +11,42 @@ See the License for the specific language governing permissions and limitations 
 */
 package co.casterlabs.commons.platform;
 
-import java.nio.ByteOrder;
 import java.util.regex.Pattern;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 @AllArgsConstructor
 public enum Arch {
     // @formatter:off
 	
     // x86/64 family.
-    X86_64    ("x86_64",  64, "amd64|x86_64"),
-    IA64      ("ia64",    64, "ia64|itanium64"),
-    X86       ("x86",     32, "x86|i[0-9]86|ia32"),
+    X86_64    ("x86_64",  "amd64|x86_64"),
+    IA64      ("ia64",    "ia64|itanium64"),
+    X86       ("x86",     "x86|i[0-9]86|ia32"),
     
     // Arm family.
-    AARCH64   ("aarch64", 64, "arm64|aarch64"), // Apple M1 chip series.
-    ARM       ("arm",     32, "arm"),
+    AARCH64   ("aarch64", "arm64|aarch64"), // Apple M1 chip series.
+    ARM       ("arm",     "arm"),
 
     // IBM's PowerPC architecture.
-    PPC64     ("ppc64",   64, "ppc64"),
-    PPC       ("ppc",     32, "ppc|power"),
+    PPC64     ("ppc64",   "ppc64"),
+    PPC       ("ppc",     "ppc|power"),
 
     // Sun's Sparc architecture.
-    SPARCV9   ("sparcv9", 64, "sparcv9|sparc64"),
-    SPARC     ("sparc",   32, "sparc"),
+    SPARCV9   ("sparcv9", "sparcv9|sparc64"),
+    SPARC     ("sparc",   "sparc"),
 
     // MIPS.
-    MIPS64   ("mips64",   64, "mips64"),
-    MIPS     ("mips",     32, "mips"),
+    MIPS64   ("mips64",   "mips64"),
+    MIPS     ("mips",     "mips"),
 
     // s390.
-    S390X   ("s390x",     64, "s390x"),
-    S390    ("s390",      32, "s390"),
+    S390X   ("s390x",     "s390x"),
+    S390    ("s390",      "s390"),
 
     // RISC-V.
-    RISCV64 ("riscv64",   64, "riscv64"),
-    RISCV   ("riscv",     32, "riscv"),
+    RISCV64 ("riscv64",   "riscv64"),
+    RISCV   ("riscv",     "riscv"),
     
  ;
     // @formatter:on
@@ -57,7 +55,6 @@ public enum Arch {
      * A standardized name for the architecture (e.g "x86_64" or "aarch64").
      */
     private String name;
-    private @Getter int wordSize;
     private String regex;
 
     static Arch get() {
@@ -80,15 +77,6 @@ public enum Arch {
     @Override
     public String toString() {
         return this.name;
-    }
-
-    /**
-     * @return whether or not the current machine's endianess is big endian.
-     * 
-     * @implNote This just calls {@link ByteOrder#nativeOrder()}.
-     */
-    public static boolean isBigEndian() {
-        return ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN;
     }
 
 }
--- Platform/src/main/java/co/casterlabs/commons/platform/Platform.java
+++ Platform/src/main/java/co/casterlabs/commons/platform/Platform.java
@@ -11,6 +11,8 @@ See the License for the specific language governing permissions and limitations 
 */
 package co.casterlabs.commons.platform;
 
+import java.nio.ByteOrder;
+
 import lombok.NonNull;
 
 public class Platform {
@@ -73,6 +75,49 @@ public class Platform {
         }
 
         return libraryName;
+    }
+
+    /**
+     * @return   whether or not the current machine's endianess is big endian.
+     * 
+     * @implNote This just calls {@link ByteOrder#nativeOrder()}.
+     */
+    public static boolean isBigEndian() {
+        return ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN;
+    }
+
+    /**
+     * Tries to detect the processor's *current* word size or "bitness."
+     * 
+     * @return   The processor's word size, or -1 if unknown. Usually 32 or 64.
+     * 
+     * @implNote Some IBM Z mainframes will return 32 even though their words are 31
+     *           bits long.
+     */
+    public static int getWordSize() {
+        // Sources:
+        // https://www.oracle.com/java/technologies/hotspotfaq.html#64bit_detection:~:text=When%20writing%20Java%20code%2C%20how%20do%20I%20distinguish%20between%2032%20and%2064%2Dbit%20operation%3F
+        // https://stackoverflow.com/a/808314
+        // https://www.ibm.com/docs/en/sdk-java-technology/8?topic=dja-determining-whether-your-application-is-running-32-bit-31-bit-z-64-bit-jvm
+
+        String SADM = System.getProperty("sun.arch.data.model");
+        if ((SADM != null) && !SADM.equals("unknown")) {
+            return Integer.parseInt(SADM);
+        }
+
+        String CIVBM = System.getProperty("com.ibm.vm.bitmode");
+        if (CIVBM != null) {
+            return Integer.parseInt(CIVBM);
+        }
+
+        String vmName = System.getProperty("java.vm.name");
+        if (vmName.contains("64-bit")) {
+            return 64;
+        } else if (vmName.contains("32-bit")) {
+            return 32;
+        }
+
+        return -1;
     }
 
 }
