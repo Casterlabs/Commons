b825f81bbbec5d9222aacaaeea4316e2b341b8c3
Ender <33337309+e3ndr@users.noreply.github.com>
2022-11-28 21:55:31 -0600

    IPC: Fix subprocess communication.
    Found a LOT of bugs in the process. Works nice and smooth now.

--- IPC/src/main/java/co/casterlabs/commons/ipc/impl/subprocess/SubprocessIpcConnection.java
+++ IPC/src/main/java/co/casterlabs/commons/ipc/impl/subprocess/SubprocessIpcConnection.java
@@ -4,13 +4,11 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.nio.ByteBuffer;
 import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 import co.casterlabs.commons.async.AsyncTask;
-import co.casterlabs.commons.async.queue.ThreadExecutionQueue;
 import co.casterlabs.commons.ipc.IpcConnection;
 import co.casterlabs.commons.ipc.packets.IpcPacket;
 import co.casterlabs.commons.ipc.packets.IpcPacket.IpcPacketType;
@@ -21,12 +19,11 @@ import lombok.SneakyThrows;
 public abstract class SubprocessIpcConnection implements Closeable {
     static final long PING_INTERVAL = TimeUnit.SECONDS.toMillis(1);
     static final long PING_TIMEOUT = PING_INTERVAL * 2;
-    private static final byte MESSAGE_PACKET_TYPE = -1;
+    private static final byte MESSAGE_PACKET_TYPE = (byte) 128;
 
-    private final ThreadExecutionQueue dispatchThread = new ThreadExecutionQueue();
     private final WrappedIpcConnection connection = new WrappedIpcConnection();
 
-    private long lastPing = System.currentTimeMillis();
+    private volatile long lastPing = System.currentTimeMillis();
 
     private OutputStream out;
     private InputStream in;
@@ -39,33 +36,7 @@ public abstract class SubprocessIpcConnection implements Closeable {
         AsyncTask.createNonDaemon(() -> {
             try {
                 while (true) {
-                    // 1) Read the length.
-                    // 2) Read the user-provided type.
-                    // 3) Read the message.
-                    // 4) Finally, read a null, so we can double check the write was good.
-                    int len = this.in.read();
-                    int type = this.in.read();
-
-                    byte[] buff = new byte[len];
-                    if (this.in.read(buff) < len) {
-                        throw new IOException("Got less bytes than expected when reading from IPC process byte stream.");
-                    }
-
-                    int expectedNull = this.in.read();
-                    if (expectedNull != 0) {
-                        throw new IOException("Expected a sanity null-termination byte. Got: " + expectedNull);
-                    }
-
-                    if (type == MESSAGE_PACKET_TYPE) {
-                        debugMessage("Received message.");
-                        JsonObject json = Rson.DEFAULT.fromJson(new String(buff), JsonObject.class);
-
-                        IpcPacket packet = IpcPacketType.get(json);
-                        this.connection.receive0(packet);
-                    } else {
-                        debugMessage("Received %d bytes of type %d.", buff.length, type);
-                        this.handleByteMessage(type, buff);
-                    }
+                    this._recvByteMessage();
                 }
             } catch (Exception e) {
                 debugError(e);
@@ -76,8 +47,13 @@ public abstract class SubprocessIpcConnection implements Closeable {
 
         // Ping thread.
         AsyncTask.createNonDaemon(() -> {
+            this.lastPing = System.currentTimeMillis();
+
             try {
                 while (true) {
+                    // Send a ping.
+                    this.connection.sendMessage(SubprocessIpcPingPacket.INSTANCE);
+
                     Thread.sleep(PING_INTERVAL);
 
                     // Check and make sure we didn't timeout.
@@ -85,9 +61,6 @@ public abstract class SubprocessIpcConnection implements Closeable {
                         this.close();
                         return;
                     }
-
-                    // Send a ping.
-                    this.connection.sendMessage(SubprocessIpcPingPacket.INSTANCE);
                 }
             } catch (Exception e) {
                 debugError(e);
@@ -110,57 +83,102 @@ public abstract class SubprocessIpcConnection implements Closeable {
 
     public abstract void handleMessage(Object message);
 
-    public abstract void handleByteMessage(int type, byte[] message);
+    public abstract void handleByteMessage(byte type, byte[] message);
 
-    public final void sendMessage(Object message) {
+    public final void sendMessage(Object message) throws IOException {
         debugMessage("Sending message.");
         this.connection.sendMessage(message);
     }
 
-    public final void sendByteMessage(byte type, byte[] message) {
+    public final void sendByteMessage(byte type, byte[] message) throws IOException {
         try {
-            assert type != MESSAGE_PACKET_TYPE : "Type " + MESSAGE_PACKET_TYPE + " is used internally.";
+            if (type == MESSAGE_PACKET_TYPE) {
+                throw new IllegalArgumentException("Type " + MESSAGE_PACKET_TYPE + " is used internally.");
+            }
+
+//            this.dispatchThread.execute(() -> {
             debugMessage("Sending %d bytes of type %d.", message.length, type);
             _sendByteMessage(type, message);
-        } catch (Exception e) {
+//            });
+        } catch (IOException e) {
             debugError(e);
-//            System.exit(1);
+            this.close();
+            throw e;
         }
     }
 
-    final synchronized void _sendByteMessage(byte type, byte[] message) throws IOException {
-        // 1) Write the length.
-        // 2) Write the user-provided type.
+    final void _recvByteMessage() throws Exception {
+        // 1) Read the user-provided type.
+        // 2) Read the length.
+        // 3) Read the message.
+        // 4) Finally, read a null, so we can double check the write was good.
+        int type = this.in.read();
+        if (type == -1) throw new IOException("Closed");
+
+        byte[] lenBytes = new byte[Integer.BYTES];
+        for (int i = 0; i < lenBytes.length; i++) {
+            int read = this.in.read();
+            if (read == -1) throw new IOException("Closed");
+            lenBytes[i] = (byte) read;
+        }
+
+        int len = ByteBuffer.wrap(lenBytes).getInt(0);
+
+        byte[] buff = new byte[len];
+        if (this.in.read(buff) < len) {
+            throw new IOException("Got less bytes than expected when reading from IPC process byte stream.");
+        }
+
+        int expectedNull = this.in.read();
+        if (expectedNull != 0) {
+            throw new IOException("Expected a sanity null-termination byte. Got: " + expectedNull);
+        }
+
+        if ((byte) type == MESSAGE_PACKET_TYPE) {
+            JsonObject json = Rson.DEFAULT.fromJson(new String(buff), JsonObject.class);
+            IpcPacket packet = IpcPacketType.get(json);
+
+            debugMessage("Received message.");
+            this.connection.receive0(packet);
+        } else {
+            debugMessage("Received %d bytes of type %d.", buff.length, type);
+            this.handleByteMessage((byte) type, buff);
+        }
+    }
+
+    synchronized final void _sendByteMessage(byte type, byte[] message) throws IOException {
+        // 1) Write the user-provided type.
+        // 2) Write the length.
         // 3) Write the message.
         // 4) Finally, write a null, so we can double check the read was good.
-        this.out.write(message.length);
         this.out.write(type);
+        this.out.write(ByteBuffer.allocate(Integer.BYTES).putInt(message.length).array());
         this.out.write(message);
         this.out.write(0);
         this.out.flush();
     }
 
     void debugError(Throwable t) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-
-        t.printStackTrace(pw);
-
-        String out = sw.toString();
-
-        pw.flush();
-        pw.close();
-        sw.flush();
-
-        debugMessage(
-            out
-                .substring(0, out.length() - 2)
-                .replace("\r", "")
-        );
+//        StringWriter sw = new StringWriter();
+//        PrintWriter pw = new PrintWriter(sw);
+//
+//        t.printStackTrace(pw);
+//
+//        String out = sw.toString();
+//
+//        pw.flush();
+//        pw.close();
+//        sw.flush();
+//
+//        debugMessage(
+//            out
+//                .substring(0, out.length() - 2)
+//                .replace("\r", "")
+//        );
     }
 
     void debugMessage(String format, Object... args) {
-        System.err.printf("[%s] %s\n", this.getClass().getSimpleName(), String.format(format, args));
+//        System.err.printf("[%s] %s\n", this.getClass().getSimpleName(), String.format(format, args));
     }
 
     class WrappedIpcConnection extends IpcConnection {
@@ -199,16 +217,16 @@ public abstract class SubprocessIpcConnection implements Closeable {
                     }
                 }
             } else {
-                dispatchThread.execute(() -> {
-                    handleMessage(message);
-                });
+                handleMessage(message);
             }
         }
 
         @SneakyThrows
         @Override
         protected void send(IpcPacket packet) {
+//            dispatchThread.execute(() -> {
             _sendByteMessage(MESSAGE_PACKET_TYPE, Rson.DEFAULT.toJsonString(packet).getBytes());
+//            });
         }
     }
 
