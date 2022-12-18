51b48f1927d8d13d2394587481e0b4b11daf1d18
Ender <33337309+e3ndr@users.noreply.github.com>
2022-12-18 07:00:42 -0600

    Platform: Get ready to implement a LOT of code for discovering ArchExtensions.

--- Platform/src/main/java/co/casterlabs/commons/platform/Platform.java
+++ Platform/src/main/java/co/casterlabs/commons/platform/Platform.java
@@ -12,6 +12,8 @@ See the License for the specific language governing permissions and limitations 
 package co.casterlabs.commons.platform;
 
 import java.nio.ByteOrder;
+import java.util.Collections;
+import java.util.List;
 
 import lombok.NonNull;
 
@@ -38,6 +40,28 @@ public class Platform {
      *           bits long.
      */
     public static final int wordSize = _getWordSize();
+
+    private static List<ArchExtensions> archExtensions;
+
+    /**
+     * The CPU Architecture extensions supported by the host, e.g x86_64 or armhf.
+     * 
+     * @implNote The lookup for this information can be a consuming task, hence why
+     *           this value isn't retrieved statically. The value is cached so that
+     *           calling getArchExtensions() multiple times will not affect
+     *           performance.
+     * 
+     * @apiNote  If a simple check will suffice, consider using the other
+     *           information available. For example, you can figure out if you're
+     *           running on x86_64 by doing
+     *           {@code archFamily == X86 && wordSize == 64}.
+     */
+    public static List<ArchExtensions> getArchExtensions() {
+        if (archExtensions == null) {
+            archExtensions = Collections.unmodifiableList(ArchExtensions.get());
+        }
+        return archExtensions;
+    }
 
     /* ---------------- */
     /* Operating System */
